0db41d5b4e9ef9df640e7212a22708ce636b3986
Chen Bin <chenbin.sh@gmail.com>
2019-04-02 21:12:57 +1100

    emergency fix of broken 0.1.4

--- org-mime.el
+++ org-mime.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Chen Bin (redguardtoo)
 ;; Keywords: mime, mail, email, html
 ;; Homepage: http://github.com/org-mime/org-mime
-;; Version: 0.1.4
+;; Version: 0.1.5
 ;; Package-Requires: ((emacs "24.4") (cl-lib "0.5"))
 
 ;; This file is not part of GNU Emacs.
@@ -387,8 +387,7 @@ CURRENT-FILE is used to calculate full path of images."
                           html)))
             files))
 
-    (when images
-      (insert (org-mime-multipart html (mapconcat 'identity images "\n"))))
+    (insert (org-mime-multipart html (if images (mapconcat 'identity images "\n"))))
 
     ;; Attach any residual files
     (when files
